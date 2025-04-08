de7a5686a7f8921c1be47a923291ff7d0384e931
KHON Kimly <142431204+kimly11@users.noreply.github.com>
2025-04-08 12:48:45 +0700

    84

--- includes/header.php
+++ includes/header.php
@@ -17,6 +17,8 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
 
     <!-- CSS Links -->
     <link rel="stylesheet" href="<?php echo APPURL; ?>/css/open-iconic-bootstrap.min.css">
@@ -47,39 +49,55 @@
             <li class="nav-item active"><a href="<?php echo APPURL; ?>" class="nav-link">Home</a></li>
             
             <!-- Admission Dropdown -->
-            <li class="nav-item dropdown">
+            <!-- <li class="nav-item dropdown">
               <a class="nav-link" href="<?php echo APPURL;?>/About.php" id="navbarDropdownMenuLink" role="button" aria-expanded="false">About</a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
               
                 <li><a href="supportstaff.php" class="dropdown-item">support staff</a></li>
               </ul>
+            </li> -->
+            <li class="nav-item dropdown">
+              <a class="nav-link" href="<?php echo APPURL;?>/About.php" id="navbarDropdownMenuLink" role="button" aria-expanded="false">About</a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              
+              <li class="has-dropright">
+                      <a href="#" id="degree"><i class="fas fa-graduation-cap"></i>Support Staff</a>
+                      <ul class="drop-right">
+                      </ul>
+                  </li>
+              </ul>
             </li>
           
             <!-- Academic Dropdown -->
             <li class="nav-item dropdown">
-              <a href="<?php echo APPURL;?>/academic.php" class="nav-link" id="navbarDropdownMenuLink" role="button" aria-expanded="false">Academic</a>
+              <a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" aria-expanded="false"  >Academics</a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <!-- DEGREE Dropdown -->
-                <li class="dropdown-submenu">
-                  <a href="#" class="dropdown-item dropdown-toggle">DEGREE</a>
-                  <ul class="dropdown-menu">
-                    <li><a href="degree.php" class="dropdown-item">PROFESSIONAL/SHORT COURSE</a></li>
-                    <li><a href="#" class="dropdown-item">ASSOCIATE</a></li>
-                    <li><a href="#" class="dropdown-item">MASTER'S DEGREE</a></li>
-                    <li><a href="bachelor.php" class="dropdown-item">BACHELOR</a></li>
-                    <li><a href="doctoral.php" class="dropdown-item">DOCTORAL</a></li>
-
-                  </ul>
-                </li>
-
-                <!-- FACULTIES Dropdown -->
-                <li class="dropdown-submenu">
-                  <a href="faculties.php" class="dropdown-item dropdown-toggle">FACULTIES</a>
-                  <ul class="dropdown-menu">
-                    <li><a href="technology.php" class="dropdown-item">FACULTY OF SCIENCE AND TECHNOLOGY</a></li>
-                    <li><a href="#" class="dropdown-item">FACULTY OF BUSINESS AND TOURISM</a></li>
-                  </ul>
-                </li>
+                <li class="has-dropright">
+                  <a href="#" id="degree"><i class="fas fa-graduation-cap"></i> DEGREE</a>
+                    <ul class="drop-right">
+                        <li><a href="professional.php" id="professional"><i class="fas fa-chalkboard-teacher"></i> PROFESSIONAL/ SHORT COURSE</a></li>                        
+                        <li><a href="associate.php" id="associate"><i class="fas fa-user-graduate"></i> ASSOCIATE</a></li>                          
+                        <li><a href="master_degree.php" id="masters-degree"><i class="fas fa-university"></i> MASTER'S DEGREE</a></li>                         
+                        <li><a href="bachelor.php" id="bachelor"><i class="fas fa-user-graduate"></i> BACHELOR</a></li>                        
+                        <li><a href="doctoral.php" id="doctoral"><i class="fas fa-award"></i> DOCTORAL</a></li>
+                      </ul>
+                  </li>
+                  
+                  
+                  <li class="has-dropright">
+                      <a href="faculty.php" id="faculties"><i class="fas fa-building"></i> FACULTIES</a>
+                      <ul class="drop-right">
+                          <li><a href="cse.php" id="faculty-science-it"><i class="fas fa-flask"></i> FACULTY OF SCIENCE AND INFORMATION TECHNOLOGY</a></li>
+                          
+                          <li><a href="Business.php" id="faculty-business-tourism"><i class="fas fa-briefcase"></i> FACULTY OF BUSINESS AND TOURISM</a></li>
+                     
+                          <li><a href="law.php" id="faculty-law-economics"><i class="fas fa-balance-scale"></i> FACULTY OF LAW AND ECONOMICS</a></li>
+                        
+                          <li><a href="education.php" id="faculty-education"><i class="fas fa-book"></i> FACULTY OF EDUCATION SCIENCE</a></li>
+                         
+                          <li><a href="#" id="faculty-news-views"><i class="fas fa-newspaper"></i> FACULTY OF NEWS AND VIEWS</a></li>
+                      </ul>
+                  </li>
               </ul>
             </li>
 
@@ -125,8 +143,41 @@
     </nav>
     <!-- END nav -->
 </body>
+<!-- <style>
+  .dropdown-menu a {
+  padding: 10px;
+  display: block;
+  white-space: nowrap;
+}
+.dropdown:hover .dropdown-menu {
+  display: block;
+}
+.drop-right {
+  display: none;
+  position: absolute;
+  left: 100%;
+  top: 0;
+  background-color: white;
+  min-width: 150px;
+  box-shadow: 2px 2px 8px rgba(138, 255, 164, 0.2);
+  z-index: 1;
+}
+.drop-right li {
+  display: block;
+}
+.drop-right li a {
+  padding: 10px;
+  display: block;
+}
+.drop-right li a:hover {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+.has-dropright:hover .drop-right {
+  display: block;
+}
+</style> -->
     <!-- JavaScript for Dropdown Menu Behavior -->
-    <script>
+    <!-- <script>
       document.querySelectorAll('.dropdown-submenu a.dropdown-toggle').forEach(function(element) {
         element.addEventListener('click', function(e) {
           e.preventDefault();
@@ -135,4 +186,4 @@
           submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
         });
       });
-    </script>
+    </script> -->
